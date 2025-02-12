c553f772f0cf9cef24af5dfe6d1600b0d943ea52
Jackstar1212 <42300522+Jackstar1212@users.noreply.github.com>
2025-02-12 11:52:46 +0800

    Update MDT_main.bat to v5.1

--- MDT_main.bat
+++ MDT_main.bat
@@ -1,4 +1,42 @@
+::[Bat To Exe Converter]
+::
+::fBE1pAF6MU+EWH/eyMf00p6kpIBa3ON17DPqJW0sKm9idcrThd4ybIrelLGWJYA=
+::fBE1pAF6MU+EWH/eyMf00p6kpIBa3ON17DPqJW0sKm9idcrThd4Pa6rD37fAJfgWig==
+::fBE1pAF6MU+EWH/eyMf00p6kpIBa3ON17DPqJW0sKm9idcrThd4xbJzflLGWJYA=
+::fBE1pAF6MU+EWH/eyMf00p6kpIBa3ON17DPqJW0sKm9idcrThd48fI2V36yLQA==
+::YAwzoRdxOk+EWAjk
+::fBw5plQjdCmDJNxADsfAwJ+MrIxN1u1MyzDLLW0eOG1Jc3EcVec1fsLU277fLa1DuhSqfJUitg==
+::YAwzuBVtJxjWCl3EqQJgSA==
+::ZR4luwNxJguZRRnk
+::Yhs/ulQjdF65
+::cxAkpRVqdFKZSDk=
+::cBs/ulQjdF25
+::ZR41oxFsdFKZSTk=
+::eBoioBt6dFKZSDk=
+::cRo6pxp7LAbNWATEpCI=
+::egkzugNsPRvcWATEpSI=
+::dAsiuh18IRvcCxnZtBJQ
+::cRYluBh/LU+EWAnk
+::YxY4rhs+aU+IeA==
+::cxY6rQJ7JhzQF1fEqQJlZksaHErSXA==
+::ZQ05rAF9IBncCkqN+0xwdVsBAlTMbCXqZg==
+::ZQ05rAF9IAHYFVzEqQK17OHRvOwG5Is91W6bMxdzIAABVICYgye6nVhcFGXu
+::eg0/rx1wNQPfEVWB+kM9LVsJDCmXMH+zIrwd79z04OaMq1lTUfo6GA==
+::fBEirQZwNQPfEVWB+kM9LVsJDCmmCCW/HrB8
+::cRolqwZ3JBvQF1fEqQK17OHRvOwG5Is91W6bMxdzIAABVICYgye6nVhcFGXu
+::dhA7uBVwLU+EWDk=
+::YQ03rBFzNR3SWATElA==
+::dhAmsQZ3MwfNWATE2E0+LRdNag2RNAs=
+::ZQ0/vhVqMQ3MEVWAtB9wSA==
+::Zg8zqx1/OA3MEVWAtB9wSA==
+::dhA7pRFwIByZRRnk
+::Zh4grVQjdCmDJNxADsfAwJ+MrIxN1u1MyzDLLW0eOG1Jc3EwcNYpLcGLlLGWJYA=
+::YB416Ek+ZG8=
+::
+::
+::978f952a14a936cc963da21a135fa983
 @echo off
+REM Copyright (c) 2025 Jackstar1212. MIT License.
 setlocal enabledelayedexpansion
 title MultiDiagToolKit
 rem check active code page flag
